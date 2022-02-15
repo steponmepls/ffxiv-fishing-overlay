f3b1c2ccd94d60f3747c0ee79f8e365c36c63eb3
steponmepls <pettan@tuta.io>
2022-02-15 01:09:53 +0100

    Fixed borked formatting in the timer and added exit events so that timer values goes back to zero

--- events.js
+++ events.js
@@ -9,8 +9,18 @@ const events = {
     /^Nothing bites/,
     /^You (reel in|put away) your/,
     /^The fish sense something amiss/
+  ],
+  success: /^You land (?:an?|\d) .(.+) measuring ([0-9.]+) (\w+)!$/,
+  exit: [
+    /^You (reel in|put away) your/,
+    /^The fish sense something amiss/
   ]
 };
+
+addOverlayListener("ChangeZone", (e) => {
+  const zone = e.zoneID;
+  console.log(`Current zone ID: ${zone}`)
+})
 
 addOverlayListener("LogLine", (e) => {
   const newLine = e.line;
@@ -18,6 +28,7 @@ addOverlayListener("LogLine", (e) => {
     const chatLog = newLine[4];
     if (events.start.test(chatLog)) {
       console.debug(`Start the timer now! - Logline: ${chatLog}`);
+      timer.innerText = 0.0.toFixed(1);
       start = Date.now();
       newTimer = window.setInterval(updateTimer, 100);
     } else {
@@ -27,6 +38,18 @@ addOverlayListener("LogLine", (e) => {
           clearTimer();
           break // Exit loop
         }
+      };
+      if (events.success.test(chatLog)) {
+        const fish = chatLog.match(events.success);
+        const elapsed = document.getElementById("timer").innerText;
+        console.log (`Fish: ${fish[1]} - Size: ${fish[2]} - Unit: ${fish[3]} - Time: ${elapsed}s`)
+      };
+      for (const rule of events.exit) {
+        if (rule.test(chatLog)) {
+          const timer = document.getElementById("timer");
+          timer.innerText = 0.0.toFixed(1);
+          break
+        }
       }
     }
   }
@@ -34,18 +57,15 @@ addOverlayListener("LogLine", (e) => {
 
 function updateTimer() {
   const timer = document.getElementById("timer");
-  const raw = Date.now() - start / 1000;
+  const raw = (Date.now() - start) / 1000;
   const formatted = raw.toFixed(1);
-  timer.innerText = formatted;
+  timer.innerText = formatted
 }
 
 function clearTimer() {
   if (newTimer) {
-    const timer = document.getElementById("timer");
     window.clearInterval(newTimer);
-    timer.innerText = 0.0;
     newTimer = null;
-    start = 0
   }
 ;}
 
