2876ed544f66e9e1da40d805eb91d26579c271e7
steponmepls <pettan@tuta.io>
2022-02-16 01:52:29 +0100

    Matching regex now dispatches custom events+payload according to their type. Also tweaked a bunch of names

--- events.js
+++ events.js
@@ -1,9 +1,9 @@
-let start = 0, newTimer, currentZone = 0, currentSpot;
+let timerStart = 0, newTimer, currentZone;
 
-const overlay = document.getElementById("container");
-const title = document.getElementById("fishing-spot");
+const container = document.getElementById("container");
+const spotTitle = document.getElementById("fishing-spot");
 const timer = document.getElementById("timer");
-const list = document.getElementById("list");
+const spotFishes = document.getElementById("list");
 
 const events = {
   start: /^You cast your line (?:in|at(?: the)?) (.+)\.$/,
@@ -15,7 +15,7 @@ const events = {
     /^You (reel in|put away) your/,
     /^The fish sense something amiss/
   ],
-  success: /^You land (?:an?|\d) .(.+) measuring ([0-9.]+) (\w+)!$/,
+  success: /^You land (an?|\d) .(.+) measuring ([0-9.]+) (\w+)!$/,
   exit: [
     /^You put away your/,
     /^The fish sense something amiss/
@@ -25,79 +25,117 @@ const events = {
 addOverlayListener("ChangeZone", (e) => {
   const newZone = e.zoneID;
   currentZone = newZone;
-  console.log(`Current zone ID: ${currentZone}`)
-})
+  // console.debug(`Current zone ID: ${currentZone}`)
+});
 
 addOverlayListener("LogLine", (e) => {
-  const newLine = e.line;
-  if (newLine.length > 4 && newLine[0] == "00") { // Only if chat log line
-    const chatLog = newLine[4];
+  const newLog = e.line;
+  if (newLog.length > 4 && newLog[0] === "00") {
+    const chatLog = newLog[4];
     if (events.start.test(chatLog)) {
-      currentSpot = chatLog.match(events.start)[1];
-      populateEntries(currentSpot);
-      overlay.classList.add("show");
-      console.debug(`Start the timer now! - Logline: ${chatLog}`);
-      timer.innerText = 0.0.toFixed(1);
-      start = Date.now();
-      newTimer = window.setInterval(updateTimer, 100);
+      const startCast = new CustomEvent("startCasting", {
+        detail: {
+          name: chatLog.match(events.start)[1]
+        }
+      });
+      document.dispatchEvent(startCast)
     } else {
-      for (const rule of events.stop) {
-        if (rule.test(chatLog) && newTimer) {
-          console.debug(`Stop the timer now! - Logline: ${chatLog}`);
-          clearTimer();
-          break // Exit loop
+      for (const regex of events.stop) {
+        if (regex.test(chatLog)) {
+          const stopCast = new CustomEvent("stopCasting");
+          document.dispatchEvent(stopCast);
+          break
+        }
+      };
+      for (const regex of events.exit) {
+        if (regex.test(chatLog)) {
+          const stopFishing = new CustomEvent("stopFishing");
+          document.dispatchEvent(stopFishing);
+          break
         }
       };
       if (events.success.test(chatLog)) {
+        let total;
         const fish = chatLog.match(events.success);
         const elapsed = timer.innerText;
-        console.log (`Fish: ${fish[1]} - Size: ${fish[2]} - Unit: ${fish[3]} - Time: ${elapsed}s`)
-      };
-      for (const rule of events.exit) {
-        if (rule.test(chatLog)) {
-          overlay.removeAttribute("class");
-          timer.innerText = 0.0.toFixed(1);
-          break
+        if (/\d/.test(fish[1])) {
+          total = parseInt(fish[1])
+        } else {
+          total = 1
         }
+        const fishCaught = new CustomEvent("fishCaught", {
+          detail: {
+            name: fish[2],
+            time: elapsed,
+            size: fish[3],
+            unit: fish[4],
+            amount: total
+          }
+        });
+        document.dispatchEvent(fishCaught)
       }
     }
   }
 });
 
-function updateTimer() {
-  const timer = document.getElementById("timer");
-  const raw = (Date.now() - start) / 1000;
-  const formatted = raw.toFixed(1);
-  timer.innerText = formatted
+document.addEventListener("startCasting", startCasting);
+document.addEventListener("stopCasting", clearTimer);
+document.addEventListener("stopFishing", () => {
+  container.removeAttribute("class");
+  timer.innerText = 0.0.toFixed(1) // 0.toFixed(1) will fail!
+});
+document.addEventListener("fishCaught", (e) => {
+  const fishName = e.detail.name;
+  const fishTime = e.detail.time;
+  const fishSize = e.detail.size;
+  const sizeUnit = e.detail.unit;
+  // console.debug(`Caught ${fishName} (${fishSize}${sizeUnit}) in ${fishTime}s`);
+});
+
+function startCasting(spot) {
+  timerStart = Date.now();
+  populateEntries(spot.detail.name);
+  timer.innerText = 0.0.toFixed(1); // 0.toFixed(1) will fail!
+  container.classList.add("show");
+  newTimer = window.setInterval(updateTimer, 100);
+}
+
+function populateEntries(name) {
+  // Pre-filter fishing log so you only search in relevant area
+  const spots = Object.entries(fishingLog).find(zone => zone[0] == parseInt(currentZone));
+  const regex = new RegExp(`${name}`, "i");
+  for (const spot of spots[1]) {
+    if (regex.test(spot.name)) {
+      spotTitle.innerText = spot.name;
+      // Clear list before adding new entries
+      while (spotFishes.firstChild) {
+        spotFishes.removeChild(spotFishes.lastChild);
+      }
+      // Fetch spot fishes and append them
+      for (const fish of spot.fishes) {
+        const name = fish.name;
+        const icon = fish.icon;
+        const newItem = document.createElement("div");
+        newItem.classList.add("fish");
+        newItem.innerHTML = `<img src="https://xivapi.com${icon}"><div class="label">${name}</div>`;
+        spotFishes.appendChild(newItem)
+      }
+      break
+    }
+  }
 }
 
 function clearTimer() {
   if (newTimer) {
     window.clearInterval(newTimer);
-    newTimer = null;
+    newTimer = null
   }
-;}
+}
 
-function populateEntries(spotName) {
-  const spots = Object.entries(fishingLog).find(zone => zone[0] == parseInt(currentZone));
-  const regex = new RegExp(`${spotName}`, "i");
-  for (const spot of spots[1]) { // Directly iterate through nested array of spots
-    if (regex.test(spot.name)) {
-      title.innerText = spotName[0].toUpperCase() + spotName.substring(1); // Force capitalize first letter ex: west Agelyss River
-      while (list.firstChild) {
-        list.removeChild(list.lastChild);
-      }
-      for (fish of spot.fishes) {
-        const name = fish.name;
-        const icon = fish.icon;
-        const newEntry = document.createElement("div");
-        newEntry.classList.add("fish");
-        newEntry.innerHTML = `<img src="https://xivapi.com${icon}"><div class="label">${name}</div>`;
-        list.appendChild(newEntry)
-      }
-      break
-    }
-  }
+function updateTimer() {
+  const raw = (Date.now() - timerStart) / 1000;
+  const formatted = raw.toFixed(1);
+  timer.innerText = formatted
 }
 
 startOverlayEvents()
