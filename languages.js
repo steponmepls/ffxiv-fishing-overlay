0ec7a458988c69bbf742e6d0df97a125cf4975c0
steponmepls <pettan@tuta.io>
2022-02-23 14:44:22 +0100

    Separated regex lines with lang variants into a specific file so that it should be easier to read and edit

---
+++ languages.js
@@ -0,0 +1,53 @@
+const languages = {
+  English: {
+    show: [
+      /^You cast your line.+\.$/,
+      /^You cast your line.+Undiscovered Fishing Hole\.$/i
+    ],
+    stat: [
+      /^.+You (gain|lose) the effect of .{2}(.+)\.$/,
+      /gain/
+    ],
+    spot: [
+      /^Data on .+ is added to your fishing log\.$/
+    ],
+    stop: [
+      /^Something bites/,
+      /^The fish gets away/,
+      /^Nothing bites/,
+      /^You reel in your line/,
+    ],
+    loot: [
+      /^You land (an?|\d) .(.+) measuring ([0-9.]+) (\w+)!$/
+    ],
+    hide: [
+      /^You put away your/,
+      /^You reel in your line/,
+      /^The fish sense something amiss/
+    ]
+  },
+  French: {
+    show: [],
+    stat: [],
+    spot: [],
+    stop: [],
+    loot: [],
+    hide: []
+  },
+  German: {
+    show: [],
+    stat: [],
+    spot: [],
+    stop: [],
+    loot: [],
+    hide: []
+  },
+  Japanese: {
+    show: [],
+    stat: [],
+    spot: [],
+    stop: [],
+    loot: [],
+    hide: []
+  }
+}
