fc82823deb5984c47d740b59adf351a09e224a95
steponmepls <pettan@tuta.io>
2022-02-28 14:27:57 +0100

    Added language key

--- js/events.js
+++ js/events.js
@@ -1,10 +1,21 @@
 "use strict";
 
-let lang, zone;
+let lang, nameLang, zone;
 
 // Get language from ACT and use "English" as fallback
 callOverlayHandler({ call: 'getLanguage' })
-.then(res => lang = ("language" in res) ? res.language : "English");
+.then(res => {
+  lang = ("language" in res) ? res.language : "English";
+  if (lang == "English") {
+    nameLang = "en"
+  } else if (lang == "German") {
+    nameLang = "de"
+  } else if (lang == "French") {
+    nameLang = "fr"
+  } else if (lang == "Japanese") {
+    nameLang = "ja"
+  }
+})
 
 // Report when you change character
 addOverlayListener("ChangePrimaryPlayer", (e) => {
