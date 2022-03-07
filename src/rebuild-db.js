0283460ccd19b4b27671894fb781f1ea2ddb4acd
steponmepls <pettan@tuta.io>
2022-03-07 19:40:26 +0100

    Investigating missing entries

--- src/rebuild-db.js
+++ src/rebuild-db.js
@@ -15,8 +15,7 @@ function initDatabase(totalSpots) {
     if (typeof totalSpots === "number") {
       interval = setInterval(sendReq, 1000)
     } else { // Error from XIVAPI
-      console.error("XIVAPI is down or you are banned gg..");
-      return
+      throw new Error("XIVAPI is down or you are banned gg..")
     }
   });
 
@@ -42,48 +41,47 @@ function fetchSpot(s) {
   fetch("https://xivapi.com/FishingSpot/" + s, { mode: "cors" })
   .then(res => res.json())
   .then(data => {
-  	const spot = data;
-  	if (spot.TerritoryTypeTargetID && spot.PlaceName.Name) {
-      const zoneID = spot.TerritoryTypeTargetID;
-      if (!(zoneID in fishingLog)) { fishingLog[zoneID] = {} }
-      // const zoneName = spot.TerritoryType.PlaceName.Name
-      const spotID = spot.ID;
-      if (!(spotID in fishingLog[zoneID])) { fishingLog[zoneID][spotID] = {} }
-      const spotNameEN = spot.PlaceName.Name_en;
-      const spotNameDE = spot.PlaceName.Name_de;
-      const spotNameFR = spot.PlaceName.Name_fr;
-      const spotNameJA = spot.PlaceName.Name_ja;
-      const fishes = [];
-      for (let i=0; i<9; i++) {
-      	const key = "Item" + i;
-      	if (spot[key] !== null) {
-        	const item = spot[key];
-        	const fishID = item.ID;
-          const fishIcon = item.Icon;
-          const fishIconHD = item.IconHD;
-          const fishNameEN = item.Name_en;
-          const fishNameDE = item.Name_de;
-          const fishNameFR = item.Name_fr;
-          const fishNameJA = item.Name_ja;
-          fishes.push({
-          	id: fishID,
-            icon: fishIcon,
-            iconHD: fishIconHD,
-            name_en: fishNameEN,
-            name_de: fishNameDE,
-            name_fr: fishNameFR,
-            name_ja: fishNameJA
-          })
-        } else {
-        	break
-        }
+    if (!(data.TerritoryTypeTargetID || data.PlaceName.Name)) return;
+
+    const spot = data;
+    const zoneID = spot.TerritoryTypeTargetID;
+    if (!(zoneID in fishingLog)) { fishingLog[zoneID] = {} }
+    // const zoneName = spot.TerritoryType.PlaceName.Name
+    const spotID = spot.ID;
+    if (!(spotID in fishingLog[zoneID])) { fishingLog[zoneID][spotID] = {} }
+    const spotNameEN = spot.PlaceName.Name_en,
+          spotNameDE = spot.PlaceName.Name_de,
+          spotNameFR = spot.PlaceName.Name_fr,
+          spotNameJA = spot.PlaceName.Name_ja,
+          fishes = [];
+    for (let i=0; i<9; i++) {
+      const key = "Item" + i;
+      if (spot[key] !== null) {
+        const item = spot[key],
+              fishID = item.ID,
+              fishIcon = item.Icon,
+              fishIconHD = item.IconHD,
+              fishNameEN = item.Name_en,
+              fishNameDE = item.Name_de,
+              fishNameFR = item.Name_fr,
+              fishNameJA = item.Name_ja;
+
+        fishes.push({
+          id: fishID,
+          icon: fishIcon,
+          iconHD: fishIconHD,
+          name_en: fishNameEN,
+          name_de: fishNameDE,
+          name_fr: fishNameFR,
+          name_ja: fishNameJA
+        })
       }
-      fishingLog[zoneID][spotID].name_en = spotNameEN;
-      fishingLog[zoneID][spotID].name_de = spotNameDE;
-      fishingLog[zoneID][spotID].name_fr = spotNameFR;
-      fishingLog[zoneID][spotID].name_ja = spotNameJA;
-      fishingLog[zoneID][spotID].fishes = fishes
     }
+    fishingLog[zoneID][spotID].name_en = spotNameEN;
+    fishingLog[zoneID][spotID].name_de = spotNameDE;
+    fishingLog[zoneID][spotID].name_fr = spotNameFR;
+    fishingLog[zoneID][spotID].name_ja = spotNameJA;
+    fishingLog[zoneID][spotID].fishes = fishes
   })
 }
 
